e35a75f3d1138e6ae7a50cf62034eb7aa941ea50
zendoctrine <168051790+zendoctrine@users.noreply.github.com>
2024-05-01 16:48:38 -0700

    Update declaration.h
    Declarations updated for completion of part G and H

--- EXAM2/declaration.h
+++ EXAM2/declaration.h
@@ -16,7 +16,6 @@ void yearSetUp(MyCalendar* c);
 void monthSetup(MyCalendar* c);
 void daySetUp(MyCalendar* c);
 void calendarSetUp(MyCalendar* c);
-void scheduleAndReport();
-void saveCalendar();
-void restoreCalendar();
-
+void scheduleAndReport(MyCalendar* c);
+void saveCalendar(MyCalendar* c);
+void restoreCalendar(MyCalendar* c);
