ce5ea572611d7a1141597ababd67d5817e510bf3
zendoctrine <168051790+zendoctrine@users.noreply.github.com>
2024-05-02 18:37:03 -0700

    Update declaration.h

--- EXAM2/declaration.h
+++ EXAM2/declaration.h
@@ -6,14 +6,16 @@
 #include <fstream>
 #include <sstream>
 #include <ctime>
+#include <iomanip>
 #include "input.h"
 #include "MyUnit.h"
 #include "MyScheduleDate.h"
 #include "MyCalendar.h"
-
+#include <string>
 
 char menuOption(MyCalendar*);
 
+void monthArray(MyCalendar* c, int(&validDay)[7][5]);
 void yearSetUp(MyCalendar* c);
 void monthSetup(MyCalendar* c);
 void daySetUp(MyCalendar* c);
@@ -21,4 +23,3 @@ void calendarSetUp(MyCalendar* c);
 void scheduleAndReport(MyCalendar* c);
 void saveCalendar(MyCalendar* c);
 void restoreCalendar(MyCalendar* c);
-
