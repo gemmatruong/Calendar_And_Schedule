a9eb1d9fa55450267dae77a4d7f2e0e30d8ae3f6
zendoctrine <168051790+zendoctrine@users.noreply.github.com>
2024-05-02 18:37:22 -0700

    Update MyCalendar.h

--- EXAM2/MyCalendar.h
+++ EXAM2/MyCalendar.h
@@ -24,12 +24,14 @@ public:
 	void setCurrentDay(int m);
 	unsigned short getCurrentDay() const;
 
+	MyScheduleDate getSDay(int m, int d);
 	unsigned short getDaysInMonth() const;
 
 	string yearToWords() const;
-
 	string getMonthName() const;
 	string getDayOfWeek() const;
+	string updateDaySuffix() const;
+
 	int* getSystemDate() const;
 	int updateDaysInMonth();
 
@@ -42,17 +44,16 @@ public:
 	void jumpForward(int);			// jump forward a number of days
 	void jumpBackward(int);			// jump forward a number of days
 
-	string updateDaySuffix() const;
 
 	void scheduleDate(int day, const std::string& description);
 	void unscheduleDate(int day);
 	void displayYearSchedules() const;
 	void displayMonthSchedules() const;
 	void displayDaySchedule() const;
-	MyScheduleDate& getScheduleDate(int month, int day); // New function to access specific schedule dates
+	MyScheduleDate& getScheduleDate(); //int month, int day New function to access specific schedule dates
+	void updateToSystemDate();
 
 	friend ostream& operator<<(ostream& out, const MyCalendar& obj);
 
-	void updateToSystemDate();
 
 };
